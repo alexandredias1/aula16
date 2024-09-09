ba5928b6922f835494c49df1c8d5502670e09132
alexandre dias <xanderio@outlook.com>
2024-09-09 18:50:14 -0300

    Initial commit

---
+++ aplicativo/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.Write("Escolha a forma que deseja viajar: \n1 - Uber\n2 - Taxi\n3 - UberMoto ");
+int escolha = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+switch (escolha)
+{
+    case 1:
+    Console.WriteLine("A forma escolhida foi Uber");
+        break;
+    case 2:
+        Console.WriteLine("A forma escolhida foi Taxi");
+        break;
+    case 3:
+        Console.WriteLine("A forma escolhida foi UberMoto");
+        break;
+    default:
+        Console.WriteLine("Opção invalida.");
+        break;
+}
+
+
+Console.ReadKey();
---
+++ aula16/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Podrão!");
+
+Console.Write("Digite o seu nome: ");
+string cliente = Console.ReadLine();
+
+Console.Write("Qual o nome do item: ");
+string item = Console.ReadLine();
+
+Console.Write("Qual a quantidade: ");
+int quantidade = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("O valor total é de: ");
+float valor = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine($"O cliente {cliente}, pediu o total de {quantidade} {item}");
+Console.WriteLine($"O valor total é de {valor}");
+
+
+Console.ReadKey();
